162ec5a6b1f25bc19f13d27a5342dc3dbe7e8b17
PennyLIU2022 <114879207+PennyLIU2022@users.noreply.github.com>
2022-11-14 22:43:11 -0500

    updated code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,77 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+let schedule = localStorage.schedule ? JSON.parse(localStorage.schedule):
+ [ {'Hour':'9AM', 'Schedule':'' },
+   {'Hour':'10AM', 'Schedule':'' },
+   {'Hour':'11AM', 'Schedule':'' },
+   {'Hour':'12PM', 'Schedule':'' },
+   {'Hour':'1PM', 'Schedule':'' },
+   {'Hour':'2PM', 'Schedule':'' },
+   {'Hour':'3PM', 'Schedule':'' },
+   {'Hour':'4PM', 'Schedule':'' },
+   {'Hour':'5PM', 'Schedule':'' },
+ ]
+ const scheduleEl = document.querySelector("#schedule");
+ var alertEl = document.querySelector("#alert")
+
+// set and show current time under header
+var timeDisplayEl = $('#currentDay');
+function displayTime() {
+  var currentTime = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(currentTime);
+}
+
+// Creat div for each item in schedule array
+function showschedule(){
+  scheduleEl.innerHTML='';
+  schedule.forEach(function(item,index) {
+    scheduleEl.innerHTML+=`<div class="row time-block">
+    <div class="col-2 col-md-1 hour text-center py-3" class="hour">  ${item.Hour}
+    </div>
+
+    <textarea id="${index}" class="${getTime(item)} col-8 col-md-10 description" onfocus="this.select()">
+    ${item.Schedule}</textarea>
+
+    <div class="saveBtn col-2 col-md-1">
+      <i class="fas fa-save" onClick='saveschedule(event)'></i>
+    </div>
+
+    </div>`
+  });
+}
+
+showschedule();
+
+function getTime(item){
+  let currentTime = dayjs().hour(12);    // get current hour
+  console.log(currentTime);
+  let scheduleTime = Number(item.Hour.slice(0,-2));// Numeric time on schedule
+  let scheduleTimeAMPM = item.Hour.substr(-2);// AM/PM of time on schedule
+  let convertedTime;
+  //convert time-block to 24-hours format
+  if(scheduleTimeAMPM == "AM"){
+      convertedTime = scheduleTime;
+  }else{
+      convertedTime = scheduleTime+12;
+  }
+  //reutrn class name based on currentTime
+  if(convertedTime<currentTime) return 'past';
+  else if(convertedTime==currentTime) return 'present';
+  else if(convertedTime>currentTime) return 'future';
+}
+
+function saveschedule(event){
+  event.preventDefault();
+  let textArea = event.target.parentElement.parentElement.childNodes[3];
+  let idx = textArea.id;
+  // save input to schedule
+  schedule[idx].Schedule = textArea.value;
+  // save to local storage
+  localStorage.schedule = JSON.stringify(schedule);
+  alert("The new schedule is added to the local storage!")
+  showSchedule();
+}
+
+displayTime();
+setInterval(displayTime, 1000);
